f9a078c7a3dfc1b16f990aded179c6c96c919077
mcb273 <mcb273@cornell.edu>
2021-04-30 17:23:02 -0400

    fixed front end

--- app/static/validation.js
+++ app/static/validation.js
@@ -1,39 +1,39 @@
-$(document).ready(function() {
+// $(document).ready(function () {
 
-  $("#resort-recommendations").on("submit", function (e) {
+//   $("#resort-recommendations").on("submit", function (e) {
 
-    var valid = true;
+//     var valid = true;
 
-    if( $("#location").prop("validity").valid ) {
-      $("#locationError").addClass("hidden");
-    } else {
-      $("#locationError").removeClass("hidden");
-      valid = false;
-    }
+//     if ($("#location").prop("validity").valid) {
+//       $("#locationError").addClass("hidden");
+//     } else {
+//       $("#locationError").removeClass("hidden");
+//       valid = false;
+//     }
 
-    if( $("#distance").prop("validity").valid ) {
-      $("#distanceError").addClass("hidden");
-    } else {
-      $("#distanceError").removeClass("hidden");
-      valid = false;
-    }
+//     if ($("#distance").prop("validity").valid) {
+//       $("#distanceError").addClass("hidden");
+//     } else {
+//       $("#distanceError").removeClass("hidden");
+//       valid = false;
+//     }
 
-    if( $("#description").prop("validity").valid ) {
-      $("#descriptionError").addClass("hidden");
-    } else {
-      $("#descriptionError").removeClass("hidden");
-      valid = false;
-    }
+//     if ($("#description").prop("validity").valid) {
+//       $("#descriptionError").addClass("hidden");
+//     } else {
+//       $("#descriptionError").removeClass("hidden");
+//       valid = false;
+//     }
 
-    if( valid==true ) {
-      e.preventDefault();
-      $("#inputs").hide();
-      $("#results").show();
-      $("#results").removeClass("hidden");
-    }
+//     // if (valid == true) {
+//     //   e.preventDefault();
+//     //   $("#inputs").hide();
+//     //   $("#results").show();
+//     //   $("#results").removeClass("hidden");
+//     // }
 
-    return valid;
+//     return valid;
 
-  });
+//   });
 
-});
+// });
